80b224510148c49a10e9fcbfab49efa955eaa724
bertonilo <bertoniloo420@gmail.com>
2022-10-18 19:58:37 +0200

    fixed related songs in artists details

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
@@ -9,20 +10,16 @@ const ArtistDetails = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-
-
-    if (isFetchingArtistDetails) return
-    <Loader title="Searching artists details" />;
+    if (isFetchingArtistDetails) return <Loader title="Loading artist details..." />;
 
     if (error) return <Error />;
 
     return (
         <div className="flex flex-col">
             <DetailsHeader
-             artistId={artistId}
-             artistData={artistData} />
-
-
+                artistId={artistId}
+                artistData={artistData}
+            />
 
             <RelatedSongs
                 data={Object.values(artistData?.songs)}
